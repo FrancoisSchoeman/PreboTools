bc479c3b97beb6b673f6fc1c3159077dc9209a13
Coderperson101 <richardtbohnen@yahoo.com>
2026-06-26 15:12:38 +0200

    fixed databale pagination layout on mobile view

--- frontend/components/DataTable.tsx
+++ frontend/components/DataTable.tsx
@@ -170,7 +170,7 @@ export function DataTable<TData, TValue>({
           className="max-w-sm"
         />
       </div>
-      <div className="rounded-md border">
+      <div className="rounded-md border overflow-x-auto">
         <Table>
           <TableHeader>
             {table.getHeaderGroups().map((headerGroup) => (
